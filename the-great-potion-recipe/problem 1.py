a99eba8a5ddff5325d7a71b496cde450eb92be1f
Vhina <soloalvhin@gmail.com>
2025-05-01 12:13:39 +0800

    problem 1

--- the-great-potion-recipe/problem 1.py
+++ the-great-potion-recipe/problem 1.py
@@ -2,43 +2,45 @@ first_line = []
 second_line = []
 unique = True
 
-first_line_input = input().split(" ")
-second_line_input = input().split(" ")
+first_line_input = input().split()
+second_line_input = input().split()
 
 def invalid_number(number):
-    if number > 1000 or number < 1:
-        return True
-    else:
-        return False
-     
+    return number > 1000 or number < 1
+
 try:
-    for i in first_line_input:
-        first_line.append(int(i))
+    # Convert inputs to integers
+    first_line = [int(i) for i in first_line_input]
+    second_line = [int(i) for i in second_line_input]
 
-    for i in second_line_input:
-        second_line.append(int(i))
+    # Validate: first line must contain exactly 2 numbers
+    if len(first_line) != 2:
+        exit()
 
+    n, m = first_line
+
+    # Validate number limits
+    if invalid_number(n) or invalid_number(m):
+        exit()
+
+    # Validate length of second_line
+    if m != len(second_line):
+        exit()
+
+    # Sort and check for uniqueness
     second_line.sort()
+    for i in range(1, len(second_line)):
+        if second_line[i] == second_line[i - 1]:
+            exit()
 
-    for i in range(len(second_line)):
-        if second_line[i] == second_line[i-1]:
-            unique = False
+    # Ensure the max number to check is not less than any number in the list
+    if n < second_line[-1]:
+        exit()
 
-    if invalid_number(first_line[0]) or invalid_number(first_line[1]):
-        print("number must not be greater than 100 and less than 1")
-    elif first_line[0] <= first_line[1] or first_line[0] > first_line[1]:
-        print("enter valid inputs")
-    elif first_line[0] < second_line[-1]:
-        print("enter valid inputs")
-    elif first_line[1] != len(second_line):
-        print("enter valid inputs")
-    elif len(first_line) != 2:
-        print("enter valid inputs")
-    elif unique:
-        for i in range(first_line[0]):
-            if i + 1 not in second_line:
-                print(i+1)
-    else:
-        print("numbers must be unique")                      
+    # Output missing numbers in range 1 to n that are not in second_line
+    for i in range(1, n + 1):
+        if i not in second_line:
+            print(i)
+
 except:
-    print("enter valid inputs")
+    exit()
